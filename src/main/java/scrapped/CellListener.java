dd349fcdc268aaa1f771a043ab33cb59fb6ac648
peter <petervat@stud.ntnu.no>
2023-04-12 20:32:14 +0200

    fjerna scrapped

--- src/main/java/scrapped/CellListener.java
+++
@@ -1,6 +0,0 @@
-package project;
-
-
-public interface CellListener {
-    void cellChanged(Cell cell);
-}
--- src/main/java/scrapped/IGameSaveHandler.java
+++
@@ -1,8 +0,0 @@
-package project;
-
-import java.io.FileNotFoundException;
-
-public interface IGameSaveHandler {
-    public void save(String file, Game game) throws FileNotFoundException;
-    public Game load(String file) throws FileNotFoundException;
-}
