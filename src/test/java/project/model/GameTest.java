3d440ee8d815a8f156b2e10d87550efdb1bf3629
peter <petervat@stud.ntnu.no>
2023-04-12 20:29:59 +0200

    cone

--- src/test/java/project/model/GameTest.java
+++ src/test/java/project/model/GameTest.java
@@ -1,10 +1,6 @@
 package project.model;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.io.File;
-import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
